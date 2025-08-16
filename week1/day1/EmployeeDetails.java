a76a681a3e6b4b69e5bb811d2dff029c45330c6a
Priyanka S <rajisomasundaram29@gmail.com>
2025-08-16 21:48:30 +0530

    Add files via upload
    Week1

---
+++ week1/day1/EmployeeDetails.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "John Doe";
+		int age = 30;
+		float salary =55000.75f;
+		char gender = 'M';
+		boolean acceptTerms = true;
+		float rating = 4.5f;
+		String mobileNumber = "9876543210";
+		int  pinCode = 56001;
+		byte luckyNumber = 7;
+		System.out.println("The name of Employee is :" +name+ "\n"+"The age of employee is :"+age+ "\n" +"The salary is :"
+		+salary+ "\n" + "The gender is :" +gender+ "\n" + "The acceptTerms is :" +acceptTerms+ "\n" + "The rating is :" +rating+"\n"
+				+"The mobile number is :" +mobileNumber+ "\n" + "The pincode is :" +pinCode+ "\n" + "The luckyNumber is : "
+		+luckyNumber+ "\n");
+
+	}
+
+}
